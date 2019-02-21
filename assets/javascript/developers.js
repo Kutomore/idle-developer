b47ed8813837f13ee3e0f2ca2de5ec56ddfcd832
kutomore <kutomore@gmail.com>
2019-02-21 13:16:48 -0300

    Adding object oriented functionality and first developer

---
+++ assets/javascript/developers.js
@@ -0,0 +1,50 @@
+function Developer(nfirstDeveloperame, price, basePerformance, description, buffs, imageName) {
+  this.name = name;
+  this.price = price;
+  this.basePerformance = basePerformance;
+  this.description = description;
+  this.buffs = buffs;
+  this.level = 0;
+  this.speed = 1;
+  this.imageName = imageName;
+  this.performance = function() {
+    let performance = basePerformance + Math.floor((this.level * basePerformance) / 2);
+    let extra_performance = 0;
+    this.buffs.forEach(
+      function (buff) {
+        if(buff.active && buff.target === 'performance'){
+          performance += performance * buff.value;
+        }
+      }
+    );
+    performance += extra_performance;
+    return performance
+  }
+}
+
+function Buff(requiredLevel, description, target, value) {
+  this.requiredLevel = requiredLevel;
+  this.description = description;
+  this.target = target;
+  this.value = value;
+  this.active = false;
+}
+
+let developers = [
+  new Developer(
+    "Noobium Foreverintern",
+    40,
+    3,
+    "He doesn't know alot, but he tries hard!",
+    [
+      new Buff(10, 'Increase his performance by 5%', 'performance', 0.05),
+      new Buff(25, 'Increase his speed by 10%', 'speed', 0.1),
+      new Buff(50, 'Increase his writtenLines by 25%', 'performance', 0.25),
+      new Buff(100, 'Increase his speed by 40%', 'speed', 0.4),
+      new Buff(250, 'Increase his speed by 50%', 'speed', 0.5),
+      new Buff(500, 'Increase his performance by 60%', 'performance', 0.6),
+      new Buff(1000, 'Double his base performance', 'basePerformance', 1)
+    ],
+    'noobium-foreverintern'
+  )
+];
