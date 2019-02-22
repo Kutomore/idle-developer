ea32267b96eaeda6a5327190f6965bee2376ffd0
kutomore <kutomore@gmail.com>
2019-02-22 11:43:47 -0300

    Adding two new characters

--- assets/javascript/developers.js
+++ assets/javascript/developers.js
@@ -39,12 +39,44 @@ let developers = [
     [
       new Buff(10, 'Increase his performance by 5%', 'performance', 0.05),
       new Buff(25, 'Increase his speed by 10%', 'speed', 0.1),
-      new Buff(50, 'Increase his writtenLines by 25%', 'performance', 0.25),
+      new Buff(50, 'Increase his performance by 25%', 'performance', 0.25),
       new Buff(100, 'Increase his speed by 40%', 'speed', 0.4),
       new Buff(250, 'Increase his speed by 50%', 'speed', 0.5),
       new Buff(500, 'Increase his performance by 60%', 'performance', 0.6),
       new Buff(1000, 'Double his base performance', 'basePerformance', 1)
     ],
     'noobium-foreverintern'
+  ),
+  new Developer(
+    "Invisible man",
+    60,
+    4,
+    "You can't see him, but he's there... Maybe!",
+    [
+      new Buff(10, 'Increase his performance by 15%', 'performance', 0.15),
+      new Buff(25, 'Increase his performance by 25%', 'performance', 0.25),
+      new Buff(50, 'Increase his performance by 35%', 'performance', 0.35),
+      new Buff(100, 'Increase his speed by 10%', 'speed', 0.1),
+      new Buff(250, 'Increase his speed by 25%', 'speed', 0.25),
+      new Buff(500, 'Increase his performance by 12%', 'performance', 0.12),
+      new Buff(1000, 'Double his base performance', 'basePerformance', 1)
+    ],
+    'invisible-man'
+  ),
+  new Developer(
+    "Invisible woman",
+    100,
+    10,
+    "No relation with the invisible man, they look totally different!",
+    [
+      new Buff(10, 'Increase her performance by 15%', 'performance', 0.15),
+      new Buff(25, 'Increase her performance by 25%', 'performance', 0.25),
+      new Buff(50, 'Increase her performance by 35%', 'performance', 0.35),
+      new Buff(100, 'Increase her speed by 10%', 'speed', 0.1),
+      new Buff(250, 'Increase her speed by 25%', 'speed', 0.25),
+      new Buff(500, 'Increase her performance by 12%', 'performance', 0.12),
+      new Buff(1000, 'Double her base performance', 'basePerformance', 1)
+    ],
+    'invisible-woman'
   )
 ];
